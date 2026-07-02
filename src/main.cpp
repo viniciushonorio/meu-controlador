b466db675cc0191c95356df64ba00722a9392ec7
vinicius <vhonorio2003@gmail.com>
2026-07-02 07:48:06 -0300

    Edição no main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 
 int main() {
     std::cout << "Controlador de Viagens\n";
-    return 0;
 
     Cidade natal("Natal");
     Passageiro maria("Maria", &natal);  // & pega o endereço de natal
     std::cout << maria.getNome() << " esta em " << maria.getLocalAtual()->getNome() << "\n";
+    
+    return 0;
 }
